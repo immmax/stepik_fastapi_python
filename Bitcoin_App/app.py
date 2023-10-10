2fd8b25f391946c63080dd1ac258b9284739319d
Maxim Datskiy <datskiumax@gmail.com>
2023-10-10 01:00:03 -0500

    Add decorators for "/welcome" and "/about" paths

--- Bitcoin_App/app.py
+++ Bitcoin_App/app.py
@@ -4,3 +4,20 @@ import pydantic_models
 import config
 
 api = fastapi.FastAPI()
+
+response = {"Answer": "Returned by server"}
+
+
+@api.get("/")
+def index():
+    return response
+
+
+@api.get("/welcome")
+def welcome():
+    return "Welcome page"
+
+
+@api.get("/about")
+def about():
+    return "About page"
